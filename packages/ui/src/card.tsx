ce13cb9e6de29eb83be906d1180c21135014c789
nemanjam <nemanja.mitic.elfak@hotmail.com>
2023-12-27 17:06:28 +0100

    fix eslint and ts config

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 
 export function Card({
   title,
@@ -8,7 +8,7 @@ export function Card({
   title: string;
   children: React.ReactNode;
   href: string;
-}): JSX.Element {
+}): React.JSX.Element {
   return (
     <a
       className="ui-group ui-rounded-lg ui-border ui-border-transparent ui-px-5 ui-py-4 ui-transition-colors hover:ui-border-neutral-700 hover:ui-bg-neutral-800/30"
@@ -17,14 +17,12 @@ export function Card({
       target="_blank"
     >
       <h2 className="ui-mb-3 ui-text-2xl ui-font-semibold">
-        {title}{" "}
+        {title}
         <span className="ui-inline-block ui-transition-transform group-hover:ui-translate-x-1 motion-reduce:ui-transform-none">
           -&gt;
         </span>
       </h2>
-      <p className="ui-m-0 ui-max-w-[30ch] ui-text-sm ui-opacity-50">
-        {children}
-      </p>
+      <p className="ui-m-0 ui-max-w-[30ch] ui-text-sm ui-opacity-50">{children}</p>
     </a>
   );
 }
